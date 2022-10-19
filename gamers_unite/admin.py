20b2b6baa6aa8b98d2a818add1f77c5d6b1d633f
Dylan097 <DylanAldridge7@gmail.com>
2022-10-19 08:50:51 +0000

    Remove slug from post model and add approved

--- gamers_unite/admin.py
+++ gamers_unite/admin.py
@@ -6,9 +6,8 @@ from django_summernote.admin import SummernoteModelAdmin
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
 
-    prepopulated_fields = {'slug': ('title',)}
-    list_filter = ('status', 'created_on')
-    list_display = ('title', 'slug', 'status', 'created_on')
+    list_filter = ('status', 'created_on', 'approved')
+    list_display = ('title', 'status', 'created_on', 'approved')
     search_fields = ['title', 'content']
     summernote_fields = ('content')
 
