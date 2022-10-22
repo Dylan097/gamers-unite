b9ea3e0477a7b9513a1ef42bd54366f709b27cf8
Dylan097 <DylanAldridge7@gmail.com>
2022-10-22 15:02:40 +0000

    Add post viewing capabilities

--- gamers_unite/urls.py
+++ gamers_unite/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 
 
 urlpatterns = [
-    path('', views.PostList.as_view(), name='home')
+    path('', views.PostList.as_view(), name='home'),
+    path('post/<int:id>/', views.PostDetail.as_view(), name='post_detail'),
 ]
