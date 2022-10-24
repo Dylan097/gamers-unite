e937805a1b3aa475fcf0b636b502228bc63e48db
Dylan097 <DylanAldridge7@gmail.com>
2022-10-24 12:36:55 +0000

    Add account registration and login functionality

--- codestar/urls.py
+++ codestar/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
     path('', include('gamers_unite.urls'), name='gamers_unite_urls'),
+    path('accounts/', include('allauth.urls')),
 ]
