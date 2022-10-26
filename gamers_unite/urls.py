198dd89a864c265d9d4f8b5b44bf1d3c2be2a1d9
Dylan097 <DylanAldridge7@gmail.com>
2022-10-26 21:37:49 +0000

    Add path to create a new post

--- gamers_unite/urls.py
+++ gamers_unite/urls.py
@@ -5,4 +5,5 @@ from django.urls import path
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
     path('post/<int:id>/', views.PostDetail.as_view(), name='post_detail'),
+    path('new_post/', views.NewPost.as_view(), name='new_post')
 ]
