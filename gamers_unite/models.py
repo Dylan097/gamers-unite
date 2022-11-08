7698e702be75c19118963b19f39e7b0bcb8bf67e
Dylan097 <DylanAldridge7@gmail.com>
2022-11-08 10:08:59 +0000

    Remove slug field

--- gamers_unite/models.py
+++ gamers_unite/models.py
@@ -7,7 +7,6 @@ STATUS = ((0, "Draft"), (1, "Published"))
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
-    slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="gamers_posts")
     updated_on = models.DateTimeField(auto_now=True)
     content = models.TextField()
