50bec655d41ca4c1f075e6cd4d050dcf01068d6a
Dylan097 <DylanAldridge7@gmail.com>
2022-11-23 12:23:44 +0000

    Add path for following

--- gamers_unite/urls.py
+++ gamers_unite/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('post/<int:id>/', views.PostDetail.as_view(), name='post_detail'),
     path('new_post/', views.NewPost.as_view(), name='new_post'),
     path('like/<int:id>', views.PostLike.as_view(), name='post_like'),
+    path('follow/<int:id>', views.FollowUser.as_view(), name='follow'),
     path('edit/<int:id>', views.EditPost.as_view(), name='edit_post'),
     path('post/<int:id>/<int:comment_id>', views.EditComment.as_view(), name='edit_comment'),
     path('delete/<int:id>/<int:comment_id>/<str:model>', views.Delete.as_view(), name='delete')
