0ddd7dc01d71c62d6f67dd58ed0a1c138b83688b
Dylan097 <DylanAldridge7@gmail.com>
2022-11-25 17:33:31 +0000

    Add post sharing link

--- gamers_unite/urls.py
+++ gamers_unite/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
     path('post/<int:id>/', views.PostDetail.as_view(), name='post_detail'),
     path('new_post/', views.NewPost.as_view(), name='new_post'),
+    path('share_post/<int:id>', views.SharePost.as_view(), name='share'),
     path('like/<int:id>', views.PostLike.as_view(), name='post_like'),
     path('follow/<int:id>/<int:post_id>', views.FollowUser.as_view(), name='follow'),
     path('edit/<int:id>', views.EditPost.as_view(), name='edit_post'),
